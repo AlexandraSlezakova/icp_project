8063d3079af086406f9860e7141010d0be8c663c
Alexandra <alexandra.slezak@gmail.com>
2020-03-05 17:33:02 +0100

    pridavanie ulice do mapy a hladanie ulice

--- StreetMap.cpp
+++ StreetMap.cpp
@@ -2,9 +2,9 @@
 
 StreetMap::StreetMap()
 {
-    for (int x = 0; x < X; x++) {
-        for (int y = 0; y < Y; y++) {
-            Map[x][y] = new Coordinates(x, y);
+    for (auto & x : Map) {
+        for (auto & y : x) {
+            y.push_back(nullptr);
         }
     }
 }
@@ -12,9 +12,60 @@ StreetMap::StreetMap()
 
 StreetMap::~StreetMap()
 {
-    for (auto &x : Map) {
-        for (auto &y : x) {
-           delete y;
+    for (auto & x : Map) {
+        for (auto & y : x) {
+            IF(y.size() > 1, delete y.front())
+            delete y.back();
         }
     }
+}
+
+bool
+StreetMap::AddStreet(Street *s)
+{
+    int counter = 0;
+
+    for (int x = s->start->x; x <= s->end->x; x++) {
+        for (int y = s->start->y; y <= s->end->y; y++) {
+            /* only two streets can cross at a point */
+            std::vector<Street *> &positionOnMap = Map[x][y];
+            IF(positionOnMap.front() != nullptr && counter >= 0, counter++)
+
+            /* restart for loop */
+            if (counter > 1) {
+                x = s->start->y == s->end->y ? s->start->x - 1 : s->start->x;
+                y = s->start->x == s->end->x ? s->start->y - 1 : s->start->y;
+                counter = -1;
+            } /* erase street from map */
+            else if (counter == -1) {
+                IF(positionOnMap.back()->name == s->name, positionOnMap.pop_back())
+                IF(positionOnMap.empty(), positionOnMap.push_back(nullptr))
+            } /* add street on map */
+            else {
+                /* erase nullptr */
+                IF(positionOnMap.front() == nullptr, positionOnMap.erase(positionOnMap.begin()))
+                positionOnMap.push_back(s);
+            }
+        }
+    }
+
+    return counter != -1;
+}
+
+Street*
+StreetMap::GetStreet(const std::string& name)
+{
+    for (auto & x : Map) {
+        for (auto & y : x) {
+            if (!y.empty()) {
+                IF_RETURN(y.front() && y.front()->name == name, y.front())
+                /* two crossed streets */
+                if (y.size() > 1) {
+                    IF_RETURN(y.back()->name == name, y.back())
+                }
+            }
+        }
+    }
+
+    return nullptr;
 }
--- StreetMap.h
+++ StreetMap.h
@@ -1,20 +1,25 @@
 #ifndef ICP_PROJECT_STREETMAP_H
 #define ICP_PROJECT_STREETMAP_H
 
-#include "Coordinates.h"
+#include "Street.h"
 #include <iostream>
 #include <vector>
+#include <map>
 
 #define X    100
 #define Y    100
 
 class StreetMap {
 public:
-    Coordinates *Map[X][Y]{};
+    std::vector<Street*> Map[X][Y]{};
 
     StreetMap();
 
     ~StreetMap();
+
+    bool AddStreet(Street *s);
+
+    Street *GetStreet(const std::string& name);
 };
 
 #endif
